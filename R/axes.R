2b48add81b2dc40f8b03a3c620f83faf9190eff6
Aaron Wong <aaron.wong@e61.in>
2022-04-13 11:41:33 +1000

    reflow code

--- R/axes.R
+++ R/axes.R
@@ -1,13 +1,3 @@
-
-
-
-e61_y_continuous <- function(expand_bottom = 0, expand_top = 0.015, ...) {
-  scale_y_continuous(expand = ggplot2::expansion(mult = c(expand_bottom,
-                                                          expand_top)),
-                     ...)
-}
-
-
 #' Format axes e61-style
 #'
 #' @param expand_bottom How much to expand bottom axis line by
@@ -19,19 +9,31 @@ e61_y_continuous <- function(expand_bottom = 0, expand_top = 0.015, ...) {
 #'
 #' @rdname e61_axes
 
+e61_y_continuous <- function(expand_bottom = 0,
+                             expand_top = 0.015,
+                             ...) {
+    scale_y_continuous(expand = ggplot2::expansion(mult = c(expand_bottom,
+                                                            expand_top)),
+                       sec.axis = dup_axis(),
+                       ...)
+  }
+
+
 scale_y_continuous_e61 <- function(expand_bottom = 0,
-                                       expand_top = 0.015,
-                                       ...) {
+                                   expand_top = 0.015,
+                                   ...) {
 
   e61_y_continuous(expand_bottom = expand_bottom,
-                       expand_top = expand_top,
-                       ...)
+                   expand_top = expand_top,
+                   ...)
 }
 
 
 e61_x_continuous <- function(expand_left = 0,
-                                 expand_right = 0.015,
-                                 ...) {
+                             expand_right = 0.015,
+                             ...) {
+
+
 
   scale_x_continuous(expand = ggplot2::expansion(mult = c(expand_left,
                                                           expand_right)),
@@ -43,8 +45,8 @@ e61_x_continuous <- function(expand_left = 0,
 #' @export
 
 scale_x_continuous_e61 <- function(expand_left = 0,
-                                       expand_right = 0.015,
-                                       ...) {
+                                   expand_right = 0.015,
+                                   ...) {
 
   e61_x_continuous(expand_left = expand_left,
                        expand_right = expand_right,
