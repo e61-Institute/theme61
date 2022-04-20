bcaed0288adfcc7ecad2943d72998cb4e5dc22ac
Aaron Wong <aaron.wong@e61.in>
2022-04-21 09:56:11 +1000

    rewrite side-effect tests so that they use withr to create and delete a temporary directory

--- tests/testthat/test-e61_save.R
+++ tests/testthat/test-e61_save.R
@@ -2,44 +2,46 @@ test_that("Test resizing feature for PNGs", {
 
   # Create a graph that will be written to disk (and deleted afterwards)
   g <- ggplot()
-  temp_file <- paste0(tempdir(), "\\test.png")
 
-  # Test PNG with default scaling
-  e61_save(temp_file)
+  withr::with_tempdir({
+    temp_file <- "test.png"
 
-  disk_file <- magick::image_read(temp_file)
-  deets <- magick::image_info(disk_file)
+    # Test PNG with default scaling
+    e61_save(temp_file)
 
-  expected_deets <-
-    tibble::tibble(
-      format = "PNG",
-      width = 800,
-      height = 600
-    )
+    disk_file <- magick::image_read(temp_file)
+    deets <- magick::image_info(disk_file)
 
-  lapply(c("format", "width", "height"), function(x) {
-    expect_equal(deets[[x]], expected_deets[[x]])
+    expected_deets <-
+      tibble::tibble(
+        format = "PNG",
+        width = 800,
+        height = 600
+      )
+
+    lapply(c("format", "width", "height"), function(x) {
+      expect_equal(deets[[x]], expected_deets[[x]])
     })
 
-  # Test resized PNG
-  e61_save(temp_file, resize = 2)
+    # Test resized PNG
+    e61_save(temp_file, resize = 2)
 
-  disk_file <- magick::image_read(temp_file)
-  deets <- magick::image_info(disk_file)
+    disk_file <- magick::image_read(temp_file)
+    deets <- magick::image_info(disk_file)
 
-  expected_deets <-
-    tibble::tibble(
-      format = "PNG",
-      width = 1600,
-      height = 1200
-    )
+    expected_deets <-
+      tibble::tibble(
+        format = "PNG",
+        width = 1600,
+        height = 1200
+      )
 
-  lapply(c("format", "width", "height"), function(x) {
-    expect_equal(deets[[x]], expected_deets[[x]])
+    lapply(c("format", "width", "height"), function(x) {
+      expect_equal(deets[[x]], expected_deets[[x]])
+    })
+
   })
 
-  # Cleanup
-  unlink(temp_file)
 })
 
 test_that("Test support for different file formats", {
@@ -47,36 +49,36 @@ test_that("Test support for different file formats", {
   # Create a graph that will be written to disk (and deleted afterwards)
   g <- ggplot()
 
-  temp_file <- paste0(tempdir(), "\\test.svg")
+  withr::with_tempdir({
+    temp_file <- "test.svg"
 
-  e61_save(temp_file)
+    e61_save(temp_file)
 
-  disk_file <- magick::image_read(temp_file)
-  deets <- magick::image_info(disk_file)
+    disk_file <- magick::image_read(temp_file)
+    deets <- magick::image_info(disk_file)
 
-  expected_deets <-
-    tibble::tibble(
-      format = "SVG",
-      width = 768,
-      height = 576
-    )
+    expected_deets <-
+      tibble::tibble(
+        format = "SVG",
+        width = 768,
+        height = 576
+      )
 
-  lapply(c("format", "width", "height"), function(x) {
-    expect_equal(deets[[x]], expected_deets[[x]])
+    lapply(c("format", "width", "height"), function(x) {
+      expect_equal(deets[[x]], expected_deets[[x]])
+    })
+
+    ## These should fail
+
+    # SVGs should fail if user tries to resize them
+    expect_error(e61_save(temp_file, resize = 2))
+
+    # No support for non-SVG/PNG files
+    expect_error(e61_save(paste0(tempdir(), "\\text.jpg")))
+
+    # Having png or svg in the name should still trip the file format error
+    expect_error(e61_save(paste0(tempdir(), "\\png-text.jpg")))
+
   })
-
-  # Cleanup
-  unlink(temp_file)
-
-  ## These should fail
-
-  # SVGs should fail if user tries to resize them
-  expect_error(e61_save(temp_file, resize = 2))
-
-  # No support for non-SVG/PNG files
-  expect_error(e61_save(paste0(tempdir(), "\\text.jpg")))
-
-  # Having png or svg in the name should still trip the file format error
-  expect_error(e61_save(paste0(tempdir(), "\\png-text.jpg")))
 
 })
