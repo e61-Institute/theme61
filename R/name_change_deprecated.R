e4003c05b5c119c7312837ab97d0c6b2f5034bcc
Aaron Wong <aaron.wong@e61.in>
2022-11-18 14:18:35 +1100

    replace with lifecycle:: functions to give error messages when old functions are used

--- R/name_change_deprecated.R
+++ R/name_change_deprecated.R
@@ -1,32 +1,32 @@
 # This file contains the names of functions that were renamed when we appended e61 to most functions.
 
 e61_labs <- function(...) {
-  .Deprecated("labs_e61")
+  lifecycle::deprecate_stop("0.2.0", "e61_labs()", "labs_e61()")
 }
 
 e61_save <- function(...) {
-  .Deprecated("save_e61")
+  lifecycle::deprecate_stop("0.2.0", "e61_save()", "save_e61()")
 }
 
 e61_y_title_top <- function(...) {
-  .Deprecated("y_title_top_e61")
+  lifecycle::deprecate_stop("0.2.0", "e61_y_title_top()", "y_title_top_e61()")
 }
 
 e61_colour_manual <- function(...) {
-  .Deprecated("scale_colour_e61")
+  lifecycle::deprecate_stop("0.2.0", "e61_colour_manual()", "scale_colour_e61()")
 }
 
 
 e61_fill_manual <- function(...) {
-  .Deprecated("scale_fill_e61")
+  lifecycle::deprecate_stop("0.2.0", "e61_fill_manual()", "scale_fill_e61()")
 }
 
 
 e61_colour_aus <- function(...) {
-  .Deprecated("scale_colour_e61_aus")
+  lifecycle::deprecate_stop("0.2.0", "e61_colour_aus()", "scale_colour_e61_aus()")
 }
 
 
 e61_fill_aus <- function(...) {
-  .Deprecated("scale_fill_e61_aus")
+  lifecycle::deprecate_stop("0.2.0", "e61_fill_aus()", "scale_fill_e61_aus()")
 }
