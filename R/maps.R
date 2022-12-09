4a41cbb2c587e863f6c9b6643f3570c0e5b8e6ea
Aaron Wong <aaron.wong@e61.in>
2022-12-09 16:08:59 +1100

    "trbl" seems to be the usual default for things like shorthand CSS so I'd suggest switching to that

--- R/maps.R
+++ R/maps.R
@@ -6,7 +6,7 @@
 #' @details The primary purpose of this function is to add a map layer with major place
 #' names and roads from Stamen's map server to plot under geographical data.
 #' @param bbox A named vector of coordinates decribing the extent of your map,
-#' e.g \code{c(left=150.839539,bottom=-34.024779,right=151.492882,top=-33.757742)}
+#' e.g \code{c(top = -33, right = 151, bottom = -34, left = 150)}
 #' @param adjust Adjust default zoom value
 #' @param maptype Set as the default map type for the e61 style, you can change to another
 #' maptype if needed
@@ -15,35 +15,45 @@
 #' @examples
 #'
 #' library(sf)
-#' sa3 <- filter(absmapsdata::sa32021,gcc_code_2021=="1GSYD") # let's just look at Sydney
+#' library(dplyr)
+#' sa3 <- filter(absmapsdata::sa32021, gcc_code_2021 == "1GSYD") # let's just look at Sydney
 #'
 #'  ggplot(sa3) +
-#'  e61_map(bbox=c(left=min(sa3$cent_long),bottom=min(sa3$cent_lat),right=max(sa3$cent_long),top=max(sa3$cent_lat))) +
-#'  geom_point(aes(x=cent_long, y=cent_lat)) #plot points
+#'  e61_map(bbox = c(left = min(sa3$cent_long), bottom = min(sa3$cent_lat), right = max(sa3$cent_long), top = max(sa3$cent_lat))) +
+#'  geom_point(aes(x = cent_long, y = cent_lat)) #plot points
 #'
 #'
 
-e61_map <- function(bbox=c(left=150.839539,bottom=-34.024779,right=151.492882,top=-33.757742),adjust=0,
-                     maptype= "toner-lite"){
+e61_map <-
+  function(bbox = c(
+    top = -33.757742,
+    right = 151.492882,
+    bottom = -34.024779,
+    left = 150.839539
+  ),
+  adjust = 0,
+  maptype = "toner-lite") {
 
-  lon_range <-bbox[c("left","right")]
-  lat_range <- bbox[c("bottom","top")]
-  lonlength <- diff(lon_range)
-  latlength <- diff(lat_range)
-  zoomlon <- ceiling(log2(360 * 2/lonlength))
-  zoomlat <- ceiling(log2(180 * 2/latlength))
-  zoom <- max(zoomlon, zoomlat)+ceiling(adjust)
+    lon_range <- bbox[c("left", "right")]
+    lat_range <- bbox[c("bottom", "top")]
+    lonlength <- diff(lon_range)
+    latlength <- diff(lat_range)
+    zoomlon <- ceiling(log2(360 * 2 / lonlength))
+    zoomlat <- ceiling(log2(180 * 2 / latlength))
+    zoom <- max(zoomlon, zoomlat) + ceiling(adjust)
 
-  ggmap <- ggmap::get_stamenmap(bbox=bbox,zoom=zoom,maptype = maptype)
-  xmin <- attr(ggmap, "bb")$ll.lon
-  xmax <- attr(ggmap, "bb")$ur.lon
-  ymin <- attr(ggmap, "bb")$ll.lat
-  ymax <- attr(ggmap, "bb")$ur.lat
-  p <- ggmap::inset_raster(ggmap, xmin, xmax, ymin, ymax)
+    ggmap <-
+      ggmap::get_stamenmap(bbox = bbox,
+                           zoom = zoom,
+                           maptype = maptype)
+    xmin <- attr(ggmap, "bb")$ll.lon
+    xmax <- attr(ggmap, "bb")$ur.lon
+    ymin <- attr(ggmap, "bb")$ll.lat
+    ymax <- attr(ggmap, "bb")$ur.lat
+    p <- ggmap::inset_raster(ggmap, xmin, xmax, ymin, ymax)
 
-
-  return(p)
-}
+    return(p)
+  }
 
 
 
