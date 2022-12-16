2de1e7d851b98bd1bd5f55dfe9ca9a212a0196f8
Aaron Wong <aaron.wong@e61.in>
2022-12-16 14:50:40 +1100

    add dont run to chart maker, was breaking the workflow

--- R/e61_chart_maker.R
+++ R/e61_chart_maker.R
@@ -2,26 +2,31 @@
 #'
 #' @title An add-in to easily create plots with ggplot2
 #'
-#' @description Select data to be used and map variables to aesthetics to produce a chart,
-#'  customize common elements and get code to reproduce the chart.
+#' @description Select data to be used and map variables to aesthetics to
+#'   produce a chart, customise common elements and get code to reproduce the
+#'   chart.
 #'
-#' @param data a `data.frame`, you can pass a `data.frame` explicitly to the function,
-#' or choose one in global environment after loading.
+#' @param data a `data.frame`, you can pass a `data.frame` explicitly to the
+#'   function, or choose one in global environment after loading.
 #' @param controls Controls menu to be displayed. Use `NULL` to hide all menus.
-#' @param viewer Where to display the gadget: `"dialog"`,
-#'  `"pane"` or `"browser"` (see \code{\link[shiny]{viewer}}).
+#' @param viewer Where to display the gadget: `"dialog"`, `"pane"` or
+#'   `"browser"` (see \code{\link[shiny]{viewer}}).
 #'
-#' @return `NULL`. You can view code used to produce the chart, copy it or insert it in current script.
+#' @return `NULL`. You can view code used to produce the chart, copy it or
+#'   insert it in current script.
 #' @export
 #'
-#' @importFrom shiny dialogViewer browserViewer runGadget paneViewer reactiveValues
+#' @importFrom shiny dialogViewer browserViewer runGadget paneViewer
+#'   reactiveValues
 #'
 #' @examples
+#' \dontrun{
 #' # Launch e61_chart_maker in RStudio :
 #' e61_chart_maker(iris)
 #'
 #' # Launch e61_chart_maker in without data specified :
 #' e61_chart_maker()
+#' }
 
 e61_chart_maker <-
   function(
