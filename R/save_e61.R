20dce91d0960893dd192f81153d041331b06df50
Aaron Wong <aaron.wong@e61.in>
2023-02-28 16:34:07 +1100

    ordering matters here

--- R/save_e61.R
+++ R/save_e61.R
@@ -36,7 +36,7 @@ save_e61 <-
            ...) {
 
     # Add a message for the user to specify their own height to dimension graphs correctly
-    if(getOption("save_e61.message", TRUE) || is.null(height)) {
+    if(is.null(height) || getOption("save_e61.message", TRUE)) {
 
       message(paste(
         "Note: When you use", sQuote("save_e61()"), "to save images with",
