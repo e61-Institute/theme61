61f6e96e40d065086dd98ddddc5d8892d826aed7
Aaron Wong <aaron.wong@e61.in>
2023-04-11 11:31:24 +1000

    implement automatic height determination

--- R/save_e61.R
+++ R/save_e61.R
@@ -35,7 +35,12 @@
 #' @param plot Plot object to save. Defaults to the last plot displayed so
 #'   usually you do not need to provide this explicitly.
 #' @param width Plot width in cm. Defaults to 8.5.
-#' @param height Plot height in cm. Defaults to 9 but you should specify your own.
+#' @param height Plot height in cm. The function will attempt to calculate an
+#'   appropriate height based on the labels you have provided, but this is
+#'   sensitive to small changes in the graph text so you should check if the
+#'   automatic value is aesthetically appropriate (no excess whitespace).
+#'   Otherwise, the function will default a value of 9 but this is unlikely to
+#'   be appropriate.
 #' @inheritDotParams ggplot2::ggsave scale dpi
 #' @export
 
@@ -49,24 +54,62 @@ save_e61 <-
            dpi = 100,
            ...) {
 
-    # Message for the user to specify their own height to dimension graphs
-    # correctly
-    if (is.null(height) || isTRUE(getOption("save_e61.message"))) {
-
-      cli::cli_text(cli::col_red("Note: When you use ", sQuote("save_e61()"), " to save images with defaults, you should set the ", sQuote("height"), " argument manually to your own value to avoid excess/insufficient whitespace on the rendered image."))
-      cli::cli_text("Unfortunately the only way to check this is to open the rendered graphic and inspect it visually.")
-      cli::cli_text("This message is shown if you leave 'height = NULL' and may be disabled by setting options('save_e61.message' = FALSE). See ?save_e61 for more details.")
-
-      options('save_e61.message' = FALSE)
-    }
-
     if (!grepl("(\\.png|\\.svg)", filename))
       stop("You must provide a file extension. Only .svg and .png file formats are currently supported.")
 
     # Check if graph is horizontal
     flip <- isTRUE("CoordFlip" %in% class(ggplot2::ggplot_build(plot)$layout$coord))
 
-    # Set default dimensions if not otherwise specified
+    # Calculate graph height based on the graph labels for normal orientation graphs
+    if (is.null(height) && !flip) {
+
+      h <- 6.5
+
+      # Helper function that counts the number of occurrences of \n
+      n_count <- function(text)
+        nchar(text) - nchar(gsub("\n", "", text, fixed = TRUE))
+
+      # Calculate the height adjustment needed
+      if (!is.null(plot$labels$title)) {
+        t_adj <- 0.6 + n_count(plot$labels$title) * 0.3
+
+      } else {
+        t_adj <- 0
+      }
+
+      if (!is.null(plot$labels$subtitle)) {
+        st_adj <- 0.5 + n_count(plot$labels$subtitle) * 0.3
+
+      } else {
+        st_adj <- 0
+      }
+
+      if (!is.null(plot$labels$caption)) {
+        cp_adj <- 0.5 + n_count(plot$labels$caption) * 0.3
+
+      } else {
+        cp_adj <- 0
+      }
+
+    height <- h + t_adj + st_adj + cp_adj
+
+    cli::cli_text("Note: save_e61() has automatically set the height to ", height, ". Please check if this is actually appropriate for your graph.")
+
+    }
+
+    # Message for the user to specify their own height to dimension graphs
+    # correctly. This message runs after the above so when the automatic height
+    # setting is used it does not trigger.
+    if (is.null(height) || isTRUE(getOption("save_e61.message"))) {
+
+      cli::cli_text(cli::col_red("Note: When you use ", sQuote("save_e61()"), " to save images with defaults, you should set the ", sQuote("height"), " argument manually to your own value to avoid excess/insufficient whitespace on the rendered image."))
+      cli::cli_text("Unfortunately the only way to check this is to open the rendered graphic and inspect it visually.")
+      cli::cli_text("This message is shown if you leave 'height = NULL' and the automatic height functionality is not used. It may be disabled by setting options('save_e61.message' = FALSE). See ?save_e61 for more details.")
+
+      options('save_e61.message' = FALSE)
+    }
+
+    # Fallback default dimensions if not otherwise specified
     if (is.null(width) && !flip) width <- 8.5
     if (is.null(height) && !flip) height <- 9
 
