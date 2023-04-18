c39abe2d9a075dc11a64442408c7ada8efaa67d5
Aaron Wong <aaron.wong@e61.in>
2023-04-18 14:10:47 +1000

    better multipanel automatic dimensioning in save_e61(); better info msgs

--- R/save_e61.R
+++ R/save_e61.R
@@ -63,21 +63,28 @@ save_e61 <-
       stop("You must provide a file extension. Only .svg and .png file formats are currently supported.")
 
     # Check if graph is horizontal
-    flip <- isTRUE("CoordFlip" %in% class(ggplot2::ggplot_build(plot)$layout$coord))
+    is_flip <- isTRUE("CoordFlip" %in% class(ggplot2::ggplot_build(plot)$layout$coord))
 
     # Check if the graph is a multi-panel generated with mpanel_e61 by
     # exploiting the fact that those graphs have zero-length labels, while
     # labs_e61 forces the user to have at least a title (so length > 0 for
     # single panels)
-    is_multi <- !is.null(attr(plot, "panel_dim"))
+    is_multi <- !is.null(attr(plot, "panel_rows"))
 
     # For multi-panels: Adjust the width to fit the extra panels and send out user message to specify the height
-    if (is_multi && is.null(width)) width <- 8.5 * attr(plot, "panel_dim")[["rows"]]
-    if (is_multi && is.null(height))
-      cli::cli_text(cli::col_red("Note: You are saving a multi-panel graph, the default dimensions in save_e61() are designed for single-panel graphs so you must choose your own height value to ensure the graph looks appropriate when saved."))
+    if (is_multi && is.null(width)) {
+      width <- 8.5 * attr(plot, "panel_cols")
+    }
+
+    if (is_multi && is.null(height)) {
+      height <- 7.5 + 7 * (attr(plot, "panel_rows") - 1)
+
+      cli::cli_text(cli::col_red("Note: You are saving a multi-panel graph, save_e61() has automatically set the height to ", height, ", but this value may not be appropriate. Check how the saved graph file looks and adjust the height as required."))
+    }
+
 
     # Calculate graph height based on the graph labels for normal orientation graphs
-    if (is.null(height) && !flip && !is_multi) {
+    if (is.null(height) && !is_flip && !is_multi) {
 
       h <- 6.5
 
@@ -116,7 +123,7 @@ save_e61 <-
 
     height <- h + t_adj + st_adj + cp_adj + y_adj
 
-    cli::cli_text(cli::col_green("Note: save_e61() has automatically set the height to ", height, ". Please check if this is actually appropriate for your graph. You may have to adjust the value if the y-axis is particularly wide."))
+    cli::cli_text(cli::col_green("Note: save_e61() has automatically set the height to ", height, ". Please open the saved graph file and check if this is actually appropriate for your graph. You may have to adjust the value if the y-axis is particularly wide."))
 
     }
 
@@ -133,13 +140,13 @@ save_e61 <-
     }
 
     # Fallback default dimensions if not otherwise specified
-    if (is.null(width) && !flip) width <- 8.5
-    if (is.null(height) && !flip) height <- 9
+    if (is.null(width) && !is_flip) width <- 8.5
+    if (is.null(height) && !is_flip) height <- 9
 
     # When coord_flip() is used to make a plot horizontal, the default dims are
     # too small
-    if (is.null(width) && flip) width <- 17
-    if (is.null(height) && flip) height <- 12
+    if (is.null(width) && is_flip) width <- 17
+    if (is.null(height) && is_flip) height <- 12
 
     if (!is.null(resize)) {
       if (!grepl("\\.png", filename))
