21fe9464c6456d64c56d14c4a1a37e7bd11ed8fc
Aaron Wong <aaron.wong@e61.in>
2023-05-04 20:14:18 +1000

    let add_zeroline be at values other than 0

--- R/add_zeroline.R
+++ R/add_zeroline.R
@@ -1,5 +1,9 @@
 #' Adds a black line at y = 0
 #'
+#' @param y Numeric. The y-intercept of the line. Defaults to 0 since it is
+#'   called \code{add_zeroline} after all. But you can set it to 100 if you have
+#'   an index on your y-axis, for example. Or any other value if you're feeling
+#'   chaotic.
 #' @param colour String. The line is black by default but you can change it if
 #'   you need another colour.
 #' @param linewidth Numeric. Set the thickness of the line. Default (0.25)
@@ -7,6 +11,6 @@
 #'
 #' @return ggplot object
 #' @export
-add_zeroline <- function(colour = "black", linewidth = 0.25) {
-  ggplot2::geom_hline(yintercept = 0, linewidth = linewidth, colour = colour)
+add_zeroline <- function(y = 0, colour = "black", linewidth = 0.25) {
+  ggplot2::geom_hline(yintercept = y, linewidth = linewidth, colour = colour)
 }
