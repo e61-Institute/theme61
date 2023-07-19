7d8d2f0579538d73da5da84eac62ae584af0309c
Aaron Wong <aaron.wong@e61.in>
2023-07-19 14:49:05 +1000

    rename add_zeroline --> add_baseline remove defunct function for v0.6

--- R/retired_functions.R
+++ R/retired_functions.R
@@ -4,17 +4,30 @@
 # message code entirely from v0.4 onwards, as users have had ample warning to
 # fix their code.
 
-# Defunct from v0.3 -----------------------------------------------------
-# Delete this section from v0.5 onwards
 
-#' Defunct
+# Defunct from v0.7 -------------------------------------------------------
+
+#' Deprecated
 #' @export
 #' @noRd
-y_title_top_e61 <- function(...) {
-  lifecycle::deprecate_stop(when = "0.3.0", what = "y_title_top_e61()",
-                            details = "The functionality of `y_title_top_e61()`
-                              has been incorporated directly into `theme_e61()`.
-                              As a result the function has been removed from the
-                              package, and you should remove it from your code.")
-
+add_zeroline <- function(...) {
+  lifecycle::deprecate_soft(when = "0.6.0",
+                            what = "add_zeroline()",
+                            with = "add_baseline()")
 }
+
+# Defunct from vX.X -----------------------------------------------------
+
+# # Keep this as an example of a defunct function
+#
+# #' Defunct
+# #' @export
+# #' @noRd
+# y_title_top_e61 <- function(...) {
+#   lifecycle::deprecate_stop(when = "0.3.0", what = "y_title_top_e61()",
+#                             details = "The functionality of `y_title_top_e61()`
+#                               has been incorporated directly into `theme_e61()`.
+#                               As a result the function has been removed from the
+#                               package, and you should remove it from your code.")
+#
+# }
