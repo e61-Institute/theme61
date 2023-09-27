36ca3b9bfa92bc6ac380538188407efcfe743487
JackBuckley <buckleyjack4@gmail.com>
2023-09-27 10:43:31 +1000

    Updating title spacing in single panel charts

--- R/utils-plot-dims.R
+++ R/utils-plot-dims.R
@@ -1,40 +1,3 @@
-#' Get the known width of the plot
-#' plot The plot to find the heigh for
-#' is_mpanel Logical. Is the plot an mpanel chart.
-#' @noRd
-get_known_width <- function(plot, is_mpanel){
-  if(is_mpanel){
-
-    width <- attr(plot, "known_width")
-
-  } else {
-    p <- ggplot2::ggplotGrob(plot)
-
-    width <- sum(grid::convertWidth(p$widths, "cm", valueOnly = TRUE), na.rm = T)
-  }
-
-  return(width)
-}
-
-#' Get the known height of the plot
-#' plot The plot to find the height for.
-#' is_mpanel Logical. Is the plot an mpanel chart.
-#' @noRd
-get_known_height <- function(plot, is_mpanel){
-
-  if(is_mpanel){
-
-    height <- attr(plot, "known_height")
-
-  } else {
-    p <- ggplot2::ggplotGrob(plot)
-
-    height <- sum(grid::convertHeight(p$heights, "cm", valueOnly = TRUE))
-  }
-
-  return(height)
-}
-
 #' Get the widths of non-zero, non-NULL grobs
 #' ggplotGrob - The set of grobs for the plot we want to find widths for
 #' grob_name - The name of the grob you want to find the width of
@@ -87,4 +50,58 @@ get_grob_width <- function(ggplotGrob, grob_name){
   }
 
   return(width)
+}
+
+#' Get the heighs of non-zero, non-NULL grobs
+#' ggplotGrob - The set of grobs for the plot we want to find widths for
+#' grob_name - The name of the grob you want to find the width of
+#' @noRd
+get_grob_height <- function(ggplotGrob, grob_name){
+
+  grob <- ggplotGrob$grobs[which(stringr::str_detect(ggplotGrob$layout$name, paste0("^", grob_name)))]
+
+  # if it is a single grob - non faceted ggplots
+  if(!is.null(grob$name)){
+
+    if(grob$name == "NULL") {
+      height <- 0
+
+    } else if(!is.null(grob$height)){
+      height <- sum(grid::convertHeight(grob$height, "cm", valueOnly = TRUE), na.rm = T)
+
+    } else if(!is.null(grob$heights)){
+      height <- sum(grid::convertHeight(grob$heights, "cm", valueOnly = TRUE), na.rm = T)
+
+    } else {
+      height <- 0
+    }
+
+  # if it is a faceted ggplot, then return the first non-zero height - to avoid double counting
+  } else if(is.list(grob)) {
+
+    height <- 0
+
+    for(i in seq_along(grob)){
+
+      temp_grob <- grob[[i]]
+
+      if(temp_grob$name == "NULL") {
+        height <- 0
+
+      } else if(!is.null(temp_grob$height)){
+        height <- sum(grid::convertHeight(temp_grob$height, "cm", valueOnly = TRUE), na.rm = T)
+
+      } else if(!is.null(temp_grob$heights)){
+        height <- sum(grid::convertHeight(temp_grob$heights, "cm", valueOnly = TRUE), na.rm = T)
+
+      } else {
+        height <- 0
+      }
+
+      # return the first non-zero height
+      if(height != 0) break
+    }
+  }
+
+  return(height)
 }
