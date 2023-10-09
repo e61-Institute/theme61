47bee626e8191b967e5f5184d89e332234c2f788
Aaron Wong <aaron.wong@e61.in>
2023-10-09 16:16:46 +1100

    fix flipped adj

--- R/theme_e61.R
+++ R/theme_e61.R
@@ -447,7 +447,7 @@ square_legend_symbols <- function() {
 #' @import ggplot2
 #' @export
 
-format_flip <- function(x_adj = -9) {
+format_flip <- function(x_adj = 0) {
   theme(
     panel.grid.major.x = element_line(colour = e61_greylight6, linewidth = points_to_mm(0.5)),
     panel.grid.major.y = element_blank(),
