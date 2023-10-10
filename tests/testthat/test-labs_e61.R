c0f13e5bcc2b48499dcc29eb4f97859cca42dc37
Aaron Wong <aaron.wong@e61.in>
2023-10-10 11:01:32 +1100

    update labs tests

--- tests/testthat/test-labs_e61.R
+++ tests/testthat/test-labs_e61.R
@@ -25,28 +25,53 @@ test_that("Functionality in labs_e61 works", {
   lab <- labs_e61(title = "Test", x = "X", y = "Y", fill = "Fill")
   expect_equal(
     lab,
-    list(x = "X", y = "Y", fill = "Fill", title = "Test", subtitle = NULL, caption = NULL),
+    list(x = "X", y = "Y", fill = "Fill", title = "Test", subtitle = "", caption = NULL),
     ignore_attr = TRUE
   )
 
   # Test the text wrapping functionality works sensibly
-  lab <- labs_e61(title = "Test", footnote_max_char = 90L, footnotes = "A really really really really really really really really really really really really really long footnote", sources = "A really really really really really really really really really really really long source")
+  lab <-
+    labs_e61(
+      title = "Test",
+      footnote_wrap = 90L,
+      footnotes = "A really really really really really really really really really really really really really long footnote",
+      sources = "A really really really really really really really really really really really long source"
+    )
 
-  expect_equal(lab$caption, "* A really really really really really really really really really really really really\nreally long footnote\nSource: A really really really really really really really really really really really\nlong source")
+  expect_equal(lab$caption,
+               "* A really really really really really really really really really really really really\nreally long footnote\nSource: A really really really really really really really really really really really\nlong source",
+               ignore_attr = TRUE)
 
   # And that the position adjusts if you change the max width
-  lab <- labs_e61(title = "Test", footnote_max_char = 10L, footnotes = "A really really footnote", sources = "A really really long source")
+  lab <-
+    labs_e61(
+      title = "Test",
+      footnote_wrap = 10L,
+      footnotes = "A really really footnote",
+      sources = "A really really long source"
+    )
 
-  expect_equal(lab$caption, "* A really\nreally\nfootnote\nSource: A\nreally\nreally\nlong\nsource")
+  expect_equal(lab$caption,
+               "* A really\nreally\nfootnote\nSource: A\nreally\nreally\nlong\nsource",
+               ignore_attr = TRUE)
 
   # Test wrapping functionality for titles
   lab <- labs_e61(
     title = "A really really really really really really really really really really really really long title",
     subtitle = "A really really really really really really really really really really really really long title",
-    title_max_char = 65L,
-    subtitle_max_char = 75L)
+    title_wrap = 65L,
+    subtitle_wrap = 75L
+  )
 
-  expect_equal(lab$title, "A really really really really really really really really really\nreally really really long title")
-  expect_equal(lab$subtitle, "A really really really really really really really really really really\nreally really long title")
+  expect_equal(
+    lab$title,
+    "A really really really really really really really really really\nreally really really long title",
+    ignore_attr = TRUE
+  )
+  expect_equal(
+    lab$subtitle,
+    "A really really really really really really really really really really\nreally really long title",
+    ignore_attr = TRUE
+  )
 
 })
