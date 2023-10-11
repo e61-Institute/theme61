aadce38bc937d95ed6c926d1cdcb3ad0b80f8c51
Aaron Wong <aaron.wong@e61.in>
2023-10-11 16:20:17 +1100

    mysterious duplicate

--- R/save-helpers.R
+++ R/save-helpers.R
@@ -97,31 +97,6 @@ get_base_size <- function(chart_type, plot_base_size = 10){
   return(plot_base_size)
 }
 
-#' Set option to automatically open files created by \code{save_e61}
-#'
-#' These functions set and unset a session-wide option to automatically open
-#' files created by \code{save_e61}. This is useful when you want to look at the
-#' graph you have just created, such as when you are trying to figure out label
-#' locations or graph dimensions and don't want to manually navigate to the file
-#' location every time.
-#'
-#' @return This function is used for its side effects.
-#' @rdname set_open_graph
-#' @export
-set_open_graph <- function() {
-  options(open_e61_graph = TRUE)
-
-  invisible(TRUE)
-}
-
-#' @rdname set_open_graph
-#' @export
-unset_open_graph <- function() {
-  options(open_e61_graph = FALSE)
-
-  invisible(FALSE)
-}
-
 #' Counts the number of occurrences of a line break (\n)
 #'
 #' @param text The string to be parsed.
