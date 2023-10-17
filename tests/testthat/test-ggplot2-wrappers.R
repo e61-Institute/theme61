ba5285adad367b5d2e0dd1aee587bd1811b2a993
Aaron Wong <aaron.wong@e61.in>
2023-10-18 10:44:41 +1100

    suppress msgs

--- tests/testthat/test-ggplot2-wrappers.R
+++ tests/testthat/test-ggplot2-wrappers.R
@@ -1,7 +1,9 @@
 test_that("ggplot2 functions are masked by theme61", {
-  expect_message(ggsave(withr::local_tempfile(fileext = ".svg"), ggplot()), ".*Please use.*")
+  withr::local_options(list(quiet_wrap = FALSE))
 
-  expect_message(ggsave(withr::local_tempfile(fileext = ".svg"), ggplot() + labs()), ".*Please use.*")
+  suppressMessages(expect_message(ggsave(withr::local_tempfile(fileext = ".svg"), ggplot()), ".*Please use.*"))
 
-  expect_message(ggsave(withr::local_tempfile(fileext = ".svg"), ggplot() + scale_y_continuous()), ".*Please use.*")
+  suppressMessages(expect_message(ggsave(withr::local_tempfile(fileext = ".svg"), ggplot() + labs()), ".*Please use.*"))
+
+  suppressMessages(expect_message(ggsave(withr::local_tempfile(fileext = ".svg"), ggplot() + scale_y_continuous()), ".*Please use.*"))
 })
