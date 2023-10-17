27f80d0b5155ec2838d45f2698329586d7f571b0
Aaron Wong <aaron.wong@e61.in>
2023-10-18 10:44:32 +1100

    ws

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,5 +1,4 @@
 # Set up a minimal ggplot object that works with theme61::ggplot and theme61::save_e61
-
 minimal_plot <-
   ggplot(data.frame(x = c(0, 1), y = c(0, 1)), aes(x, y)) +
   geom_point()
