d4963a0228fb2b5f7ba818deab195b3a141450db
Jack Buckley <jack.buckley@e61.in>
2023-10-25 16:34:00 +1100

    deleting rplots.pdf

---
+++ R/mpanel_e61.R
@@ -0,0 +1,180 @@
+#' Create a multi-panel graph with e61 formatting
+#'
+#' @description Wrapper around \link[cowplot]{plot_grid} and \code{labs_e61} to
+#'   create multi-panel graphs with appropriate title and footer formatting.
+#'
+#'   This function is designed for creating 2x2 panel graphs, although it should
+#'   work for any arrangement of panels (e.g. 2x1, 3x2, etc.). Your mileage may
+#'   vary.
+#'
+#'   When saving multi-panel graphs using \code{save_e61()} you will need to
+#'   change (increase) the width and height arguments to reflect the larger size
+#'   of multi-panel graphs.
+#'
+#' @details \strong{Read this if your titles and stuff are getting cut off.}
+#'
+#'   The function tries to be smart when setting the values for
+#'   \code{rel_heights}, but it can be incorrect due to differences in the
+#'   amount of text in each component.
+#'
+#'   The way \code{mpanel_e61} works is that the title, subtitle, graph panels
+#'   and footers are all separate components that are then put together after
+#'   they have been independently generated. The purpose of \code{rel_heights}
+#'   is to specify what proportion of the final plot each of these components
+#'   require. As a result, if the values are too small for a given component,
+#'   part of it will get visually cut off.
+#'
+#'   The function tries to increase the amount of space given to a component if,
+#'   for example, your title has two lines, or you write a really long footnote
+#'   that spans many lines of text. But this is an inexact science. In all
+#'   likelihood, you will need to specify your own values, which why this
+#'   paragraph of text tries to explain the underlying function so you aren't
+#'   just blindly inputting numbers.
+#'
+#'   The default values for \code{rel_heights} for a 1-line title, 1-line
+#'   subtitle, 1-line footnote and 1-line sources list is \code{c(0.05, 0.05, 1,
+#'   0.1)}. If you only have a title and footnotes/sources then only 3 values
+#'   are needed: \code{c(0.05, 1, 0.1)}.
+#'
+#'   These values are all relative ratios, meaning, in the second example, the
+#'   title gets \eqn{\frac{0.05}{1.15}}, or around 4 per cent of the total graph
+#'   height. This means that if your graph panels are really tall (e.g. you make
+#'   a 3x2 multi-panel), you will need to reduce the share of the space
+#'   allocated to the titles or you will have extra whitespace (I think? Haven't
+#'   actually tested this).
+#'
+#' @param ... Plot objects to put on the panel.
+#' @inheritParams labs_e61
+#' @inheritParams cowplot::plot_grid
+#' @param title_adj Rescales the size of the title text to be slightly larger
+#'   than the titles of the subplots (default is 1.1). 2 doubles the font size.
+#' @param rel_heights A numeric vector giving the relative proportions of each
+#'   graph component (title, plots, footer (optional)). See the Details for more
+#'   detail.
+#'
+#' @return ggplot2 object
+#' @export
+#' @examples
+#'  gg <- ggplot2::ggplot() +
+#'    labs_e61(title = "Figure", y = "%") +
+#'    scale_y_continuous_e61(limits = c(0, 10, 2.5)) +
+#'    theme_e61()
+#'
+#'  mpanel_e61(gg, gg, gg, gg,
+#'    title = "Multi-panel graph title",
+#'    subtitle = "Graph subtitle",
+#'    footnotes = c("Footnote 1", "Footnote 2"),
+#'    sources = c("Source 1", "Source 2"))
+
+mpanel_e61 <-
+  function(...,
+           plotlist = NULL,
+           title = NULL,
+           subtitle = NULL,
+           footnotes = NULL,
+           sources = NULL,
+           title_max_char = 90,
+           subtitle_max_char = 90,
+           footnote_max_char = 130,
+           title_wrap = TRUE,
+           subtitle_wrap = TRUE,
+           footnote_wrap = TRUE,
+           title_adj = 1.1,
+           ncol = 2,
+           nrow = NULL,
+           rel_heights = NULL,
+           align = c("v", "none", "h", "hv"),
+           axis = c("none", "l", "r", "t", "b", "lr", "tb", "tblr")
+           ) {
+
+    # Prep header/footer text
+    lab_text <- label_wrap(
+      title = title,
+      subtitle = subtitle,
+      footnotes = footnotes,
+      sources = sources,
+      title_max_char = title_max_char,
+      subtitle_max_char = subtitle_max_char,
+      footnote_max_char = footnote_max_char,
+      title_wrap = title_wrap,
+      subtitle_wrap = subtitle_wrap,
+      footnote_wrap = footnote_wrap
+    )
+
+    # Gather the plots
+    plots <- c(list(...), plotlist)
+    if (is.null(nrow)) {
+      nrow <- ceiling(length(plots) / ncol)
+    }
+
+    # Put together the panels
+    panels <- cowplot::plot_grid(plotlist = plots,
+                             align = align,
+                             axis = axis,
+                             ncol = ncol,
+                             nrow = nrow
+                             )
+
+    # These all need to be lists
+    panels <- list(panels)
+    lab_head <- list()
+    lab_foot <- list()
+
+    lab_head$title <-
+      cowplot::ggdraw() +
+      cowplot::draw_label(
+        lab_text$title,
+        fontface = "bold",
+        x = 0.5,
+        hjust = 0.5,
+        size = 12 * title_adj
+      )
+
+    if (!is.null(lab_text$subtitle)) {
+      lab_head$subtitle <-
+        cowplot::ggdraw() +
+        cowplot::draw_label(
+          lab_text$subtitle,
+          fontface = "plain",
+          x = 0.5,
+          hjust = 0.5,
+          size = 12
+        )
+
+    }
+
+    if (!is.null(lab_text$caption)) {
+
+      lab_foot$footer <-
+        cowplot::ggdraw() +
+        cowplot::draw_label(lab_text$caption,
+                            x = 0,
+                            hjust = 0,
+                            size = 9) +
+        ggplot2::theme(plot.margin = margin(0, 0, 0, 3))
+
+    }
+
+    # Space for subtitle if required
+    sub_h <- if (!is.null(lab_text$subtitle)) 0.05 else NULL
+
+    # Adjust the footer height depending on how much text there is
+    cap_h <-
+      if (!is.null(lab_text$caption)) 0.05 * (n_count(lab_text$caption) + 1) else NULL
+
+    # Use automatically generated relative heights if the user does not specify their own
+    if (is.null(rel_heights)) rel_heights <- c(0.05, sub_h, 1, cap_h)
+
+    gg <- cowplot::plot_grid(
+      plotlist = c(lab_head, panels, lab_foot),
+      ncol = 1,
+      rel_heights = rel_heights
+    )
+
+    # Add some extra info on the multi-panel attributes
+    attr(gg, "panel_rows") <- nrow
+    attr(gg, "panel_cols") <- ncol
+
+    return(gg)
+
+}
---
+++ R/utils-labs_e61.R
@@ -0,0 +1,95 @@
+#' Label text wrapper
+#'
+#' This is an internal function that supplies the functionality to wrap label
+#' text to ensure it does not run beyond the boundaries of the graph. This
+#' function is used in functions that accept and produce graph header and footer
+#' text.
+#'
+#' The default values of the function arguments should be set in the parent
+#' function rather than here.
+#'
+#' @noRd
+label_wrap <- function(
+    title = NULL,
+    subtitle = NULL,
+    footnotes = NULL,
+    sources = NULL,
+    title_max_char = NULL,
+    subtitle_max_char = NULL,
+    footnote_max_char = NULL,
+    title_wrap = NULL,
+    subtitle_wrap = NULL,
+    footnote_wrap = NULL
+    ) {
+
+  # Stop titles from being too long by wrapping it to multiple lines
+  if (title_wrap) {
+    title <- paste(strwrap(title, width = title_max_char), collapse = "\n")
+  }
+
+  if (!is.null(subtitle)) {
+
+    if (!is.character(subtitle))
+      stop("subtitle must be a string.")
+
+    if (subtitle_wrap) {
+      subtitle <- paste(strwrap(subtitle, width = subtitle_max_char), collapse = "\n")
+    }
+  }
+
+  # Footnotes
+  if (!is.null(footnotes)) {
+
+    # Sense check inputs
+    if (!is.vector(footnotes) || !is.character(footnotes))
+      stop("footnotes must be a vector of strings.")
+
+    # Stops footnote text from spilling over the RHS of graphs if they are lengthy
+    if (footnote_wrap) {
+      footnotes <-
+        sapply(footnotes, function(x)
+          paste(strwrap(x, width = footnote_max_char), collapse = "\n"))
+    }
+
+    # Creates the correct number of asterisks
+    footnotes <- data.frame(n = seq(1, length(footnotes)), text = footnotes)
+    footnotes$n <- strrep("*", footnotes$n)
+    footnotes <- paste0(footnotes$n, " ", footnotes$text)
+
+  }
+
+  # Sources
+  if (!is.null(sources)) {
+
+    # Sense check inputs
+    if (!is.vector(sources) || !is.character(sources))
+      stop("sources must be a vector of strings.")
+
+    # Source list should be in alphabetical order
+    sources <- sort(sources)
+
+    # Construct the list of sources
+    source_list <- paste(sources, collapse = "; ")
+    sources <-
+      paste0(ifelse(length(sources) > 1, "Sources: ", "Source: "), source_list)
+
+    # Stops sources text from spilling over the RHS of graphs if they are
+    # lengthy
+    if (footnote_wrap) {
+      sources <-
+        paste(strwrap(sources, width = footnote_max_char), collapse = "\n")
+    }
+
+  }
+
+  # Put the footer text together
+  caption <- paste0(c(footnotes, sources), collapse = "\n")
+  if (caption == "") caption <- NULL # Return NULL caption if blank
+
+
+  # Return elements in a list
+  retval <- list(title = title, subtitle = subtitle, caption = caption)
+
+  return(retval)
+
+}
