f20cfaf2a44309a617bad1d8b328546fbc22718e
Aaron Wong <aaron.wong@e61.in>
2023-10-27 13:21:52 +1100

    add an info msg for the user

--- R/sec_axes_rescale.R
+++ R/sec_axes_rescale.R
@@ -52,6 +52,15 @@ sec_rescale_inv <- function(values, scale = 1, shift = 0) {
   assign("sec_axis_scale", scale, envir = t61_env)
   assign("sec_axis_shift", shift, envir = t61_env)
 
+  # Inform the user of the weirdness
+  if (getOption("sec_axis_msg", default = TRUE)) {
+    cli::cli_alert_info("Did your graph not show any change to the secondary axis? Due to weirdness, you need to run the graph code twice after making changes to the secondary axis rescaling. This message appears once per session. To view it again, run `options(sec_axis_msg = TRUE)`.",
+                        wrap = TRUE)
+
+    # Turn off the option after appearing once
+    options(sec_axis_msg = FALSE)
+  }
+
   return ((values + shift) / scale)
 }
 
