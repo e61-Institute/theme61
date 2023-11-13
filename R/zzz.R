c5c2c3fbe9c7dcbef7641c582dfb4ea3668baa0e
Aaron Wong <aaron.wong@e61.in>
2023-11-13 16:08:27 +1100

    add code to change the default colours from black

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,10 @@
+# Required for data.table to work
 .datatable.aware = TRUE
 
+# Required to setup an empty environment with this name
 t61_env <- NULL
 
+# Code inside here runs when the package is loaded with library(theme61)
 .onLoad <- function(libname, pkgname) {
 
   # Check if package is up-to-date
@@ -13,5 +16,18 @@ t61_env <- NULL
 
   # Set up package environment for things like dual y-axis
   t61_env <<- new.env()
+
+  ## Update default colours from black
+
+  # Update default colour
+  geoms_colour <- c("point", "line", "pointrange", "errorbar", "boxplot")
+  lapply(geoms_colour, \(x) {update_geom_defaults(x, aes(colour = e61_tealdark))})
+
+  # Update default fill
+  geoms_fill <- c("col")
+  lapply(geoms_fill, \(x) {update_geom_defaults(x, aes(fill = e61_tealdark))})
+
+  # Update defaults for other types
+  update_geom_defaults("ribbon", aes(fill= e61_tealdark, alpha = 0.1))
 
 }
