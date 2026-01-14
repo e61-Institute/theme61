1f423bc227aaf6648142d7c257c72e8bf45dc357
Aaron Wong <aaron.wong@e61.in>
2026-01-13 19:53:05 -0600

    Improve automated test coverage

--- tests/testthat/test-check_pkg_ver.R
+++ tests/testthat/test-check_pkg_ver.R
@@ -1,8 +1,17 @@
 test_that("Interactive prompt works", {
-  testthat::local_mocked_bindings(
-    gh = function(...) list(list(tag_name = "v0.9.0")),
-    .env = asNamespace("gh")
-  )
+  skip_if_not_installed("gh")
+
+  ns <- asNamespace("gh")
+  orig_gh <- get("gh", envir = ns)
+  unlockBinding("gh", ns)
+  assign("gh", function(...) list(list(tag_name = "v0.9.0")), ns)
+  lockBinding("gh", ns)
+  on.exit({
+    unlockBinding("gh", ns)
+    assign("gh", orig_gh, ns)
+    lockBinding("gh", ns)
+  }, add = TRUE)
+
   testthat::local_mocked_bindings(
     readline = function(...) "N",
     .env = baseenv()
